a53f56e9d8c9faf28ae83873df94e355135a42c9
fayaz-rafin <fayaz.rafin@gmail.com>
2024-02-14 20:07:02 -0500

    added custom fonts to the project

--- showdown-team-tracker/src/App.js
+++ showdown-team-tracker/src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a href="https://pokemondb.net/pokedex/rhydon"><img className="rhydon" src="https://img.pokemondb.net/sprites/black-white/normal/rhydon-f.png" alt="Rhydon"></img></a>
         <h1>Hello, Showdown Team Tracker!</h1>
-        
       </header>
     </div>
   );
